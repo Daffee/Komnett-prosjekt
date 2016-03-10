cd229069a890cf185e26ea92ec46117ff969adf2
gardhog <gard_yo@hotmail.com>
2016-03-10 02:14:36 +0100

    Added files via upload

--- MessageParser.py
+++ MessageParser.py
@@ -1,29 +1,30 @@
 
+history = ''
 
 class MessageParser():
     def __init__(self):
-
+        
+        self.income = ''
+        
         self.possible_responses = {
             'error': self.parse_error,
             'info': self.parse_info,
             'message': self.parse_message,
-            'history': self.parse_history,	
+            'history': self.parse_history,
         }
-
+    
     def parse(self, payload):
         payload = # decode the JSON object
-
+        
         if payload['response'] in self.possible_responses:
             return self.possible_responses[payload['response']](payload)
         else:
-            # Response not valid
-
+    # Response not valid
+    
     def parse_error(self, payload):
     
     def parse_info(self, payload):
-
+    
     def parse_message(self, payload):
 
     def parse_history(self, payload):
-    
-
